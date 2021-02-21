4c3a240364df4f376259214495f23a3be3777613
Bris <63515831+brisdevs@users.noreply.github.com>
2021-02-21 13:29:19 +0300

    🚀

--- models/SchemaÖrnek.js
+++ models/SchemaÖrnek.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose');
 const ÖrnekSchema = new mongoose.Schema({
     sunucuID: String,
     kullanıcıID: String,
-    codeming: String
 })
 
 module.exports = mongoose.model('ornek_data', ÖrnekSchema)
